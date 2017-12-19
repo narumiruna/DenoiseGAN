c05eab86702c97b5e126865c4fb42d6ca341d255
なるみ <4680567+narumiruna@users.noreply.github.com>
2017-12-19 22:01:33 +0800

    add google image crawler

--- utils.py
+++ utils.py
@@ -1,59 +1,69 @@
-from icrawler.builtin  import GoogleImageCrawler
-from torch.utils import data
-from PIL import Image
 import glob
 import os
-from torchvision import transforms
+
 import torch
-from torchvision import datasets
-
-datasets.MNIST
-
-transform = transforms.Compose([
-    transforms.RandomCrop(128),
-    transforms.ToTensor()
-])
+from icrawler.builtin import GoogleImageCrawler
+from PIL import Image
+from torch.utils import data
+from torchvision import datasets, transforms
 
 
-
-
-
-class GoogleImageDataset(data.Dataset):
-    def __init__(self, root, keyword, train=True, transform=None, target_transform=None, download=False):
-        super(GoogleImageDataset, self).__init__()
+class GoogleImage(data.Dataset):
+    def __init__(self,
+                 root,
+                 keyword,
+                 transform=None,
+                 download=False,
+                 min_size=[200, 200],
+                 max_num=10000,
+                 image_format='jpg'
+                 ):
+        super(GoogleImage, self).__init__()
         self.root = root
-        self.keyword=keyword
-        self.train_data = None
+        self.keyword = keyword
+        self.max_num = max_num
+        self.min_size = min_size
+        self.transform = transform
+        self.image_format = image_format
+        if download:
+            self.download()
 
-        self.max_num = 1000
+        self.train_data = self.load()
 
     def __getitem__(self, index):
-        pass
+        img = self.train_data[index]
+        if self.transform:
+            img = self.transform(img)
+        return img
+
     def __len__(self):
-        pass
+        return len(self.train_data)
 
     def download(self):
-        google_crawler = GoogleImageCrawler(parser_threads=2, downloader_threads=4,
-                                        storage={'root_dir': os.path.join(self.root, self.keyword)})
-        google_crawler.crawl(self.keyword, max_num=self.max_num,
-                            date_min=None, date_max=None,
-                            min_size=(200,200), max_size=None)
+        google_crawler = GoogleImageCrawler(parser_threads=2,
+                                            downloader_threads=4,
+                                            storage={'root_dir': os.path.join(self.root, self.keyword)})
+        google_crawler.crawl(self.keyword,
+                             max_num=self.max_num,
+                             date_min=None, date_max=None,
+                             min_size=self.min_size,
+                             max_size=None)
 
     def load(self):
-        pattern='*.{}'.format(format)
-        paths = glob.glob(os.path.join(self.root, self.keyword,pattern))
-
+        pattern = '*.{}'.format(self.image_format)
+        paths = glob.glob(os.path.join(self.root, self.keyword, pattern))
+        if not paths:
+            raise Exception('There is no image file.')
         images = []
         for path in paths:
-            img = transform(Image.open(path))
-            if img.size()[0] == 3:
-                images.append(img)
-        self.train_data = torch.stack(images)
+            image = Image.open(path)
+            # append RGB image
+            if image.mode == 'RGB':
+                images.append(image)
+        return images
 
 def main():
-    dataset = GoogleImageDataset('data', 'おっぱい')
-    dataset.download()
-    dataset.load()
+    dataset = GoogleImage('data', 'tree', download=True, max_num=100)
 
 if __name__ == '__main__':
     main()
