334219e62f1d20aa446175af01072014c9a0163f
なるみ <4680567+narumiruna@users.noreply.github.com>
2017-12-24 15:29:26 +0800

    update bugs...

--- main.py
+++ main.py
@@ -4,8 +4,122 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 from torch.autograd import Variable
+from torch.utils import data
 from torchvision import datasets, transforms
 
+from utils import ImageFolder, psnr
+
 parser = argparse.ArgumentParser()
+parser.add_argument('--cuda', action='store_true')
 parser.add_argument('--batch_size', '-bs', type=int, default=128)
+parser.add_argument('-lr', type=float, default=1e-4)
 args = parser.parse_args()
+
+
+class BasicBlock(nn.Module):
+    def __init__(self, in_ch=63, out_ch=64):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_ch, out_ch - 1, 3, 1, 1)
+        self.conv2 = nn.Conv2d(in_ch, 1, 3, 1, 1)
+
+    def forward(self, input_):
+        main = F.relu(self.conv1(input_))
+        residual = self.conv2(input_)
+        return main, residual
+
+
+class DenoiseNet(nn.Module):
+    def __init__(self):
+        super(DenoiseNet, self).__init__()
+
+        self.layer1 = BasicBlock(1, 10)
+        self.layer2 = BasicBlock(9, 10)
+        self.layer5 = nn.Conv2d(9, 1, 3, 1, 1)
+
+    def forward(self, input_):
+        output = input_
+        # first layer
+        main, residual = self.layer1(input_)
+        output += residual
+        # secibd layer
+        main, residual = self.layer2(main)
+        output += residual
+        # last layer
+        output += self.layer5(main)
+
+        return output
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 32, 5, 1, 0),
+            nn.MaxPool2d(2),
+            nn.LeakyReLU(),
+            nn.Conv2d(32, 64, 5, 1, 0),
+            nn.MaxPool2d(2),
+            nn.LeakyReLU()
+        )
+        self.linear = nn.Sequential(
+            nn.Linear(64 * 29 * 29, 1024),
+            nn.LeakyReLU(),
+            nn.Linear(1024, 1),
+        )
+
+    def forward(self, input_):
+        output = self.conv(input_)
+        output = output.view(-1, 64 * 29 * 29)
+        output = self.linear(output)
+        return output
+
+
+train_dataset = ImageFolder('data')
+train_dataloader = data.DataLoader(train_dataset, batch_size=args.batch_size)
+
+n = DenoiseNet()
+d = Discriminator()
+
+if args.cuda:
+    n.cuda()
+    d.cuda()
+
+optimizer_n = torch.optim.Adam(n.parameters(), lr=args.lr)
+optimizer_d = torch.optim.Adam(d.parameters(), lr=args.lr)
+
+
+def train(epoch):
+    for batch_idx, (noisy, real) in enumerate(train_dataloader):
+        noisy = Variable(noisy)
+        real = Variable(real)
+
+        if args.cuda:
+            noisy = noisy.cuda()
+            real = real.cuda()
+
+        denoised = n(noisy)
+        loss_d = -d(real).mean() + d(denoised).mean()
+
+        optimizer_d.zero_grad()
+        loss_d.backward()
+        optimizer_d.step
+
+        if batch_idx % 10 == 0:
+            loss_n = -d(denoised).mean()
+
+            optimizer_n.zero_grad()
+            loss_n.backward()
+            optimizer_n.step()
+
+        print(batch_idx)
+
+    gt = real.cpu().data.numpy()
+    pred = denoised.cpu().data.numpy()
+    print('psnr: {}'.format(psnr(gt[0], pred[0])))
+
+def main():
+    train(0)
+
+
+if __name__ == '__main__':
+    main()
--- utils.py
+++ utils.py
@@ -1,78 +1,75 @@
-import glob
 import os
 
-import torch
-from icrawler.builtin import GoogleImageCrawler
+import numpy as np
+import skimage
+import torchvision.transforms.functional as F
 from PIL import Image
 from torch.utils import data
-from torchvision import datasets, transforms
+from torchvision import transforms
+
+IMG_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm']
 
 
-class GoogleImage(data.Dataset):
-    def __init__(self, root, keyword, transform=None, download=False, min_size=[200, 200], max_num=10000, image_format='jpg'):
-        super(GoogleImage, self).__init__()
+def is_image_file(filename):
+    return any(filename.lower().endswith(ext) for ext in IMG_EXTENSIONS)
 
+
+def add_poisson_noise(image):
+    noisy = skimage.util.noise.random_noise(np.array(image), mode='poisson')
+    return Image.fromarray(np.uint8(noisy * 255))
+
+
+def psnr(x, y):
+    """
+    x: ground turth
+    y: noisy image
+    """
+    mse = np.array((x - y)**2).mean()
+    max_x = np.max(x)
+    psnr = 10 * np.log10(max_x**2 / mse)
+    # print('psnr: {}, mse: {}, max_x: {}'.format(psnr, mse, max_x))
+    return psnr
+
+class ImageFolder(data.Dataset):
+    def __init__(self, root, size=128):
+        super(ImageFolder, self).__init__()
         self.root = root
-        self.keyword = keyword
-        self.max_num = max_num
-        self.min_size = min_size
-        self.transform = transform
-        self.image_format = image_format
-        self.train_data = None
-        self.pt_filename = os.path.join(self.root, self.keyword+'.pt')
+        self.size = size
+        self.classes = [d for d in os.listdir(
+            root) if os.path.isdir(os.path.join(root, d))]
 
-        if download and not os.path.exists(self.pt_filename):
-            self.download()
-            self.read_and_save()
-        else:
-            self.train_data = torch.load(self.pt_filename)
+        self.real_images = []
+        for class_ in self.classes:
+            for dir_path, _, filenames in os.walk(os.path.join(root, class_)):
+                for filename in filenames:
+                    if is_image_file(filename):
+                        self.real_images.append(
+                            os.path.join(dir_path, filename))
 
     def __getitem__(self, index):
-        img = self.train_data[index]
-        if self.transform:
-            img = self.transform(img)
-        return img
+        # get crop function
+        random_crop = transforms.RandomCrop(self.size)
+
+        # get real image
+        real_image = Image.open(self.real_images[index])
+
+        # randomly crop real image
+        real_image = random_crop(real_image)
+
+        # to grayscale
+        real_image = F.to_grayscale(real_image)
+
+        # get noisy image
+        noisy_image = add_poisson_noise(real_image)
+
+        # to tensor image
+        noisy_image = F.to_tensor(noisy_image)
+        real_image = F.to_tensor(real_image)
+
+        # normalize
+        noisy_image = F.normalize(noisy_image, (0.5,), (0.5,))
+        real_image = F.normalize(real_image, (0.5,), (0.5,))
+        return noisy_image, real_image
 
     def __len__(self):
-        return len(self.train_data)
-
-    def download(self):
-        google_crawler = GoogleImageCrawler(parser_threads=20,
-                                            downloader_threads=40,
-                                            storage={'root_dir': os.path.join(self.root, self.keyword)})
-        google_crawler.crawl(self.keyword,
-                             max_num=self.max_num,
-                             date_min=None, date_max=None,
-                             min_size=self.min_size,
-                             max_size=None)
-
-    def read_and_save(self):
-        pattern = '*.{}'.format(self.image_format)
-        paths = glob.glob(os.path.join(self.root, self.keyword, pattern))
-        if not paths:
-            raise Exception('There is no image file.')
-        self.train_data = []
-        for path in paths:
-            image = Image.open(path)
-            # append RGB image
-            if image.mode == 'RGB':
-                self.train_data.append(image.copy())
-        # save
-        torch.save(self.train_data, self.pt_filename)
-
-def main():
-
-    transform = transforms.Compose([
-        transforms.CenterCrop(128),
-        transforms.ToTensor()
-    ])
-
-    dataset = GoogleImage('data', 'tree', transform=transform, download=True)
-    data_loader = torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=True)
-
-    for idx , x in enumerate(data_loader):
-        print(idx, x.size())
-
-
-if __name__ == '__main__':
-    main()
+        return len(self.real_images)
